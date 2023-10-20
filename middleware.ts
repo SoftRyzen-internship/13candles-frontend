4debb3ba21cf586df69bfdac9132654ff0420737
veronikanos <vveronikanika@gmail.com>
2023-10-20 19:15:25 +0300

    update: add type directory, refactor language button

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,5 @@ export default createMiddleware({
 });
  
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips
-  // certain folders and all pathnames with a dot (e.g. favicon.ico)
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
