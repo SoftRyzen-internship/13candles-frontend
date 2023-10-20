b634c7a75a6663f1dd24cd059ffca6b7263b0bb6
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-10-20 12:03:50 +0300

    fix misspell

--- sections/homeSections/ExampleSection/ExampleSection.tsx
+++ sections/homeSections/ExampleSection/ExampleSection.tsx
@@ -5,7 +5,7 @@ export const ExampleSection = () => (
         <h1 className="visually-hidden">I am a visually hidden heading</h1>
         <h2 className="title-lg">title-lg - e.g. Договір оферти</h2>
         <h2 className="title-md">title-md - e.g. Загальні положення</h2>
-        <h2 className="title-sm">title-md - e.g. Навігація</h2>
+        <h2 className="title-sm">title-sm - e.g. Навігація</h2>
         <h2 className="section-title-primary">
           section-title-primary - e.g. Каталог
         </h2>
