6c9a917cea9e7930f5f59a9b210786ba298e78c1
veronikanos <vveronikanika@gmail.com>
2023-10-23 17:15:53 +0300

    rename messages directory to dictionaries, change paths

--- i18n.ts
+++ i18n.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
  
 export default getRequestConfig(async ({ locale }) => ({
     messages : {
-   ...(await import(`./messages/${locale}/login.json`)).default,
-   ...(await import(`./messages/${locale}/home.json`)).default
+   ...(await import(`./dictionaries/${locale}/login.json`)).default,
+   ...(await import(`./dictionaries/${locale}/home.json`)).default
  }
  }));
