48479ead42e904a330a06948d9635b5959187964
veronikanos <vveronikanika@gmail.com>
2023-10-23 19:11:32 +0300

    parse reach text from json translation

--- sections/home/AboutSection/AboutSection.tsx
+++ sections/home/AboutSection/AboutSection.tsx
@@ -1,40 +1,31 @@
 import Image from 'next/image';
-import im from '../../../public/images/homepage/about/about@2x.webp';
-// import { useTranslations } from 'next-intl';
+import { useTranslations } from 'next-intl';
 
 export const AboutSection = () => {
-
-  // const t = useTranslations('Home');
-  const text = [
-    '13 Candle Bar - це сімейна майстерня ароматів і затишку. Наше невелике виробництво знаходиться в Київській області та спеціалізується на виготовленні аромадифузорів, ароматичних свічок, свічок в кашпо та інших предметах затишку.',
-    'Наші клієнти обирають нас через високоякісний сервіс, використання виключно натуральних і топових матеріалів і компонентів, преміальні аромати та гарне співвідношення вартості з якістю. Також ми можемо виготовляти як оптові, так і корпоративні замовлення з розробкою продукції під існуючий бренд.',
-    'Новим напрямком для нас стала ароматизація приміщень. Адже ми розуміємо, що бізнесу важливо закохувати в себе клієнтів, створюючи приємне враження за допомогою ароматів.',
-    'Пишіть нам, задавайте питання, і ми залюбки познайомимо вас з всесвітом затишку і приємних ароматів в 13 Candle Bar.',
-  ];
+  const about = useTranslations('about');
 
   return (
-    <section
-      className="section xl:py-[50px]"
-      // id={about}
-    >
+    <section className="section xl:py-[50px]" id="about">
       <div className="container">
-        <h2 className="section-title-primary md:mb-6 smOnly:mb-3">Про нас</h2>
-        <div className="gap-8 md:flex md:mb-8 xl:gap-[132px] xl:mb-12 xl:max-w-5xl xl:mx-auto">
-          <div className="flex-1 flex flex-col smOnly:mb-3">
-            <Image src={im} alt="SOMETEXT"
-            sizes="100vw" 
-              />
+        <h2 className="section-title-primary md:mb-6 smOnly:mb-3">
+          {about('title')}
+        </h2>
+        <div className="gap-8 md:mb-8 md:flex xl:mx-auto xl:mb-12 xl:max-w-5xl xl:gap-[132px]">
+          <div className="flex flex-1 flex-col smOnly:mb-3">
+            <Image src={about("image.src")} width={500} height={500} alt={about('image.alt')} sizes="100vw" />
           </div>
-          <ul className="text-justify flex-1 leading-[18.78px] xl:text-lg smOnly:mb-6">
-            {text.map((line, idx) => (
-              <li key={idx} className="mb-3 last:mb-0 last:tracking-[-0.16px]">
-                {line}
-              </li>
-            ))}
+          <ul className="flex-1 text-justify leading-[18.78px] xl:text-lg smOnly:mb-6">
+            {about.rich('content', {
+              item: chunks => (
+                <li className="mb-3 last:mb-0 last:tracking-[-0.16px]">
+                  {chunks}
+                </li>
+              ),
+            })}
           </ul>
         </div>
-        <a className="button md:w-fit md:mx-auto" href="./">
-          Каталог
+        <a className="button md:mx-auto md:w-fit" href="./">
+          {about('button')}
         </a>
       </div>
     </section>
