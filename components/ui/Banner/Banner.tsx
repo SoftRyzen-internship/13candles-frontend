16604dc93267d48be7c6826d12391f01742f47fa
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-10-24 13:03:15 +0300

    image default sizes

--- components/ui/Banner/Banner.tsx
+++ components/ui/Banner/Banner.tsx
@@ -5,8 +5,8 @@ export const Banner: React.FC<BannerProps> = ({ src, alt }) => (
   <div className="h-[50wv] w-full sm:h-[240px] md:h-[384px] xl:mx-auto xl:h-[592px] xl:w-[1184px]">
     <Image
       className="h-full w-full object-cover object-center"
-      width={1000}
-      height={450}
+      width={1184}
+      height={592}
       src={src}
       alt={alt}
       sizes="100wv"
