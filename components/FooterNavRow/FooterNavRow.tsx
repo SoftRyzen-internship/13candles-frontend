1ea31ab6839c7c1391dfabe1147349e303d8142f
Volodymyr Bondarenko <qweeqer2012@gmail.com>
2023-10-24 18:36:10 +0300

    refactor code

--- components/FooterNavRow/FooterNavRow.tsx
+++ components/FooterNavRow/FooterNavRow.tsx
@@ -1,29 +1,24 @@
 import React from 'react';
-
 import Link from 'next/link';
-
 import { FooterNavRowProps } from '@/components/FooterNavRow/types';
 
 export const FooterNavRow: React.FC<FooterNavRowProps> = ({ data }) => {
-  const navText = data.nav_text;
-  const nav = data.nav;
-
-  if (!nav || !Array.isArray(nav)) {
-    return null;
-  }
+  const { nav_text, nav } = data;
 
   return (
-    <div className="hidden xl:grid xl:gap-3 xl:text-[16px] xl:leading-5">
-      <p className="xl:font-bold">{navText}</p>
-      <ul className="xl:grid xl:gap-3">
-        {nav.map(item => (
-          <li key={item.href || item.name}>
-            <Link className="link xl:font-medium" href={item.href || '#'}>
-              {item.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    nav?.length && (
+      <div className="hidden xl:grid xl:gap-3 xl:text-[16px] xl:leading-5">
+        <p className="xl:font-bold">{nav_text}</p>
+        <ul className="xl:grid xl:gap-3">
+          {nav.map(item => (
+            <li key={item.href || item.name}>
+              <Link className="link xl:font-medium" href={item.href || '#'}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
   );
 };
