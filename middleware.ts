fb12a17d17be71a2e709b16ad735e1cc96a3a3f4
veronikanos <vveronikanika@gmail.com>
2023-10-25 11:32:37 +0300

    change image

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,7 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|public|images|icons|meta|favicon.ico).*)',
+  ],
 };
--- sections/home/AboutSection/AboutSection.tsx
+++ sections/home/AboutSection/AboutSection.tsx
@@ -8,17 +8,17 @@ export const AboutSection: React.FC<AboutProps> = ({ about }) => {
         <h2 className="section-title-primary md:mb-6 smOnly:mb-3">
           {about.title}
         </h2>
-        <div className="gap-8 md:mb-8 md:flex xl:mx-auto xl:mb-12 xl:max-w-5xl xl:gap-[132px]">
-          <div className="flex flex-1 flex-col smOnly:mb-3">
+        <div className="gap-8 md:mb-8 md:flex xl:mx-auto xl:mb-12 xl:max-w-5xl xl:gap-[132px] mdOnly:gap-[30px]">
+          <div className="flex flex-1 flex-col xl:max-h-[565px] xl:overflow-hidden smOnly:mb-4 mdOnly:align-middle">
             <Image
-              src="/public/images/homepage/about/about@2x.webp"
+              src={about.image.src}
               width={500}
               height={500}
               alt={about.image.alt}
-              sizes="100vw"
+              style={{ objectFit: 'cover' }}
             />
           </div>
-          <ul className="flex-1 text-justify leading-[18.78px] xl:text-lg smOnly:mb-6">
+          <ul className="flex-1 text-justify leading-[19px] xl:text-lg smOnly:mb-6">
             {about.content.map((p, i) => (
               <li key={i} className="mb-3 last:mb-0 last:tracking-[-0.16px]">
                 {p}
