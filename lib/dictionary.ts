7c2bf762d462367dc75c412507864b24fa98ea2b
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-10-25 10:27:59 +0300

    refactor dictionaries

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -4,9 +4,8 @@ import type { Locale } from '@/i18n.config';
 const dictionaries = {
   en: async () => ({
     ...(await import(`@/dictionaries/en/common.json`)).default,
-    ...(await import(`@/dictionaries/en/home.json`)).default,
-    ...(await import(`@/dictionaries/en/delivery_and_payment.json`)).default,
     ...(await import(`@/dictionaries/en/home_page.json`)).default,
+    ...(await import(`@/dictionaries/en/delivery_and_payment.json`)).default,
     ...(await import(`@/dictionaries/en/offer_contract.json`)).default,
     ...(await import(`@/dictionaries/en/rules_of_use.json`)).default,
     ...(await import(`@/dictionaries/en/warranties_and_returns.json`)).default,
@@ -14,9 +13,8 @@ const dictionaries = {
 
   uk: async () => ({
     ...(await import(`@/dictionaries/uk/common.json`)).default,
-    ...(await import(`@/dictionaries/uk/home.json`)).default,
-    ...(await import(`@/dictionaries/uk/delivery_and_payment.json`)).default,
     ...(await import(`@/dictionaries/uk/home_page.json`)).default,
+    ...(await import(`@/dictionaries/uk/delivery_and_payment.json`)).default,
     ...(await import(`@/dictionaries/uk/offer_contract.json`)).default,
     ...(await import(`@/dictionaries/uk/rules_of_use.json`)).default,
     ...(await import(`@/dictionaries/uk/warranties_and_returns.json`)).default,
