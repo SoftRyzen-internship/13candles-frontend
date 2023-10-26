9b0137986094f11935e87f0ac05afe5e10ce9f6d
Volodymyr-Bezyk <99117313+Volodymyr-Bezyk@users.noreply.github.com>
2023-10-26 20:20:17 +0300

    add metadata

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -24,7 +24,7 @@ export async function generateMetadata({
   const {
     common: { meta },
   } = await getDictionary(lang);
-  const { metadata, twitter, openGraph } = meta;
+  const { metadata, twitter, openGraph, icons } = meta;
   const { title, description, keywords, manifest } = metadata;
 
   return {
@@ -40,70 +40,11 @@ export async function generateMetadata({
     },
     keywords,
     manifest,
-
     twitter,
     openGraph: { ...openGraph, url: `${process.env.BASE_URL}${lang}` },
-    icons: {
-      icon: [
-        {
-          url: '/meta/favicon.ico',
-        },
-        {
-          url: '/meta/favicon-16x16.png',
-          sizes: '16x16',
-          type: 'image/png',
-        },
-        {
-          url: '/meta/favicon-32x32.png',
-          sizes: '32x32',
-          type: 'image/png',
-        },
-        {
-          url: '/meta/android-chrome-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          url: '/meta/android-chrome-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-      shortcut: '/meta/favicon.ico',
-      apple: '/meta/apple-touch-icon.png',
-      other: {
-        rel: '/meta/apple-touch-icon.png',
-        url: '/meta/apple-touch-icon.png',
-      },
-    },
+    icons,
   };
 }
-
-// "openGraph": {
-// "images": [
-//   {
-//     "url": "/images/ogp-image.jpg",
-//     "width": 1200,
-//     "height": 630,
-//     "alt": "Varicose Vein Treatment in Kyiv | Vascular Surgeon - Sergii Voitsitskyi"
-//   }
-// ],
-//   "description": "Consultation with a phlebologist. Effective varicose vein treatment, modern equipment, comfortable conditions. Affordable price. Treatment for thrombosis.",
-//   "type": "website",
-//   "locale": "en_US",
-//   "title": "Varicose Vein Treatment in Kyiv | Vascular Surgeon - Sergii Voitsitskyi",
-//   "url": "https://phlebologist.vercel.app/en"
-// }
-
-// openGraph: {
-//   title: title,
-//   description: description,
-//   url: BASE_URL as string,
-//   siteName: title,
-//   locale: locale,
-//   type: 'website',
-//   images: images,
-// },
 
 export async function generateStaticParams() {
   return i18n.locales.map(locale => ({ lang: locale }));
