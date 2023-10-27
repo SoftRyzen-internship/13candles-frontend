36215c657f435ace3a5b9c14dd5bec251243a9e7
veronikanos <vveronikanika@gmail.com>
2023-10-27 18:18:12 +0300

    add import routes to defineCurentPage

--- utils/defineCurrentPage.ts
+++ utils/defineCurrentPage.ts
@@ -1,9 +1,5 @@
+import { FOR_BUSINESS } from '@/data';
+
 export const defineCurrentPage = (pathname: string) => {
-  if (pathname.includes('/business')) {
-    return 'Business';
-  }
-  if (pathname.includes('/categories')) {
-    return 'Categories';
-  }
-  return 'Home';
+  return pathname.includes(FOR_BUSINESS) ? 'Business' : 'Home';
 };
