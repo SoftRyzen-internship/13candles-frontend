43699f526081d417b47fd152063b89bfbd79c5ee
Mariia Kovalova <mariia_kovalova@ukr.net>
2023-10-30 11:19:31 +0200

    button position on modal window

--- components/ui/ModalWindow/ModalWindow.tsx
+++ components/ui/ModalWindow/ModalWindow.tsx
@@ -18,8 +18,7 @@ export const ModalWindow: React.FC<ModalWindowPrps> = ({
   const { closeIconAriaLabel } = data;
 
   const modalClasses = classNames(
-    'container relative z-40 my-10 bg-body px-[10px] pb-6 pt-[76px] ',
-    'md:px-4 md:py-8 xl:w-[1220px] xl:px-6 xl:py-12',
+    'container relative z-40 my-10 bg-body px-[10px] pb-8 pt-[60px] md:px-4 xl:w-[1220px] xl:px-6',
     className,
   );
 
@@ -30,18 +29,18 @@ export const ModalWindow: React.FC<ModalWindowPrps> = ({
           type="button"
           aria-label={closeIconAriaLabel[lang]}
           onClick={onModalClose}
-          className="absolute right-[20px] top-[36px] z-20 h-7 w-7 md:right-[32px] xl:right-[60px] xl:top-[50px]"
+          className="absolute right-[14px] top-[20px] z-20 h-fit w-fit md:right-[26px] xl:right-[40px]"
         >
           <IconClose
             width={28}
             height={28}
-            className="xl:h-12 xl:w-12 xl:opacity-25"
+            className="xl:h-10 xl:w-10 xl:opacity-25"
           />
         </button>
         <div
-          className="max-h-[calc(100vh-80px-24px-76px)] overflow-auto px-[10px] 
-                    md:max-h-[calc(100vh-80px-(32px*2))] md:px-4
-                    xl:max-h-[calc(100vh-80px-(48px*2))] xl:px-6"
+          // content height = calc(100vh - margin and padding of the modal window)
+          // this way the scroll bar is inside the modal window
+          className="max-h-[calc(100vh-80px-60px-32px)] overflow-auto px-[10px] md:px-4 xl:px-6"
         >
           {children}
         </div>
