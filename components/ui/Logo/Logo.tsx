7301f08e18277abd7ded412a417b8c1db8d220a5
i-havr <i.havrylov@gmail.com>
2023-10-30 21:16:54 +0200

    fix logo link

--- components/ui/Logo/Logo.tsx
+++ components/ui/Logo/Logo.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useParams } from 'next/navigation';
 import classNames from 'classnames';
 import { Locale } from '@/i18n.config';
@@ -10,14 +12,14 @@ export const Logo: React.FC<LogoProps> = ({
   aria = '',
   className = '',
 }) => {
-  const lang = useParams()?.lang as Locale;
+  const lang = useParams().lang as Locale;
   const logoSvgStyles = classNames('h-[48px] w-[87px]', {
     'xl:w-[116px] xl:h-[64px]': position === 'header',
     'md:w-[116px] md:h-[64px]': position === 'footer' || 'contacts',
   });
   return (
     <a
-      href={lang ? `/${lang}` : '/'}
+      href={`/${lang}`}
       aria-label={aria}
       className={`common-transition block max-w-max text-black-light hover:text-black focus:text-black ${className}`}
     >
