8ea8717a5237fb98dc29f114f2098d9e6cf7dcc9
yevhen-petrunkin <novamovaxxi@gmail.com>
2023-10-30 11:12:13 +0200

    add usefulwhom section

--- app/[lang]/business/page.tsx
+++ app/[lang]/business/page.tsx
@@ -3,6 +3,7 @@ import { Locale } from '@/i18n.config';
 
 import { SecondSection } from '@/sections/business/SecondSection';
 import { HelpfulSection } from '@/sections/business/HelpfulSection';
+import { UsefulWhomSection } from '@/sections/business/UsefulWhomSection';
 
 export default async function BusinessPage({
   params: { lang },
@@ -10,12 +11,13 @@ export default async function BusinessPage({
   params: { lang: Locale };
 }) {
   const { businessPage } = await getDictionary(lang);
-  const { secondSection, helpfulSection } = businessPage;
+  const { secondSection, helpfulSection, usefulWhom } = businessPage;
 
   return (
     <main>
       <SecondSection {...secondSection} />
       <HelpfulSection {...helpfulSection} />
+      <UsefulWhomSection {...usefulWhom} />
     </main>
   );
 }
