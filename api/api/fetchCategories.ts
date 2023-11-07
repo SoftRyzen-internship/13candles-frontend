fe5f512aabede8fa39fc94cac94c6a0a2a04eedf
Nadiia <holineinadia@gmail.com>
2023-11-07 15:29:56 +0200

    add categories from strapi and connect at page

---
+++ api/api/fetchCategories.ts
@@ -0,0 +1,24 @@
+import { request } from 'graphql-request';
+
+import { Locale } from '@/i18n.config';
+
+import { getCategories } from '../requests/getCategories';
+import { CategoriesData, CategoriesDataType } from '@/types/Categories';
+
+export const fetchCategories = async (
+  locale: Locale,
+): Promise<CategoriesData> => {
+  try {
+    const data: CategoriesDataType = await request(
+      process.env.API_BASE_URL as string,
+      getCategories,
+      { locale: locale },
+    );
+
+    const result = data.categories.data;
+    return result;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
--- sections/home/CatalogSection/CatalogSection.tsx
+++ sections/home/CatalogSection/CatalogSection.tsx
@@ -1,15 +1,31 @@
 import { CategoriesList } from '@/components/CategoriesList';
-import { CatalogSectionProps } from './types';
 import { CATALOG } from '@/data';
 
-export const CatalogSection: React.FC<CatalogSectionProps> = ({
-  catalog: { title, subtitle, categories },
-}) => (
-  <section id={CATALOG} className="section xl:pb-[50px] xl:pt-[15px]">
-    <div className="container">
-      <h2 className="section-title-primary mb-2">{title}</h2>
-      <p className="mb-6 text-md font-medium text-black-light/25">{subtitle}</p>
-      <CategoriesList categories={categories} />
-    </div>
-  </section>
-);
+import { fetchCategories } from '@/api/api/fetchCategories';
+
+import { CatalogSectionProps } from './types';
+
+export const CatalogSection: React.FC<CatalogSectionProps> = async ({
+  catalog: { title, subtitle, categories: staticCategories },
+  lang,
+}) => {
+  const categories = await fetchCategories(lang);
+
+  return (
+    <section id={CATALOG} className="section xl:pb-[50px] xl:pt-[15px]">
+      <div className="container">
+        <h2 className="section-title-primary mb-2">{title}</h2>
+
+        <p className="mb-6 text-md font-medium text-black-light/25">
+          {subtitle}
+        </p>
+
+        {categories && categories.length > 0 ? (
+          <CategoriesList categories={categories} />
+        ) : (
+          <CategoriesList categories={staticCategories} />
+        )}
+      </div>
+    </section>
+  );
+};
