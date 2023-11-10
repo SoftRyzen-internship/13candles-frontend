9dde2f1debbf31c7850a782e404e512eb787250c
Nadiia <holineinadia@gmail.com>
2023-11-10 14:19:11 +0200

    look for logs

--- api/fetchAromas.ts
+++ api/fetchAromas.ts
@@ -13,6 +13,8 @@ export const fetchAromas = async (locale: Locale): Promise<AromasData> => {
       { locale: locale },
     );
 
+    console.log(request);
+
     const result = data.aromas.data;
     return result;
   } catch (error) {
--- app/[lang]/[category]/page.tsx
+++ app/[lang]/[category]/page.tsx
@@ -51,6 +51,8 @@ export async function generateStaticParams({
     return { lang: lang, category: category.slug };
   });
 
+  console.log(staticParams);
+
   return staticParams;
 }
 
